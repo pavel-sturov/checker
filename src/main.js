c356ce15685506e88150f949f6c5e2418a8bc15e
Pavel Sturov <pavel.sturau@winlinetech.net>
2023-12-13 15:57:51 +0300

    Added start message.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@ const mailer = require('./utils/mail-transporter')
 const url     = 'https://nomadgoods.com/products/sport-band-ultra-strike?_data=routes%2F%28%24lang%29._frame.products.%24handle'
 const TIMEOUT = 10 * 1000
 
-const mailOptions = {
-  from:    'nodejs.257@gmail.com',
-  to:      'gdedat@gmail.com',
+const startOptions = {
+  subject: 'Сервер запущен.',
+  html:    '<h1>Сервер запущен, как только ремешок появится - вы получите уведомление.</h1>',
+}
+
+const successOptions = {
   subject: 'Это случилось!!!',
   html:    '<h1>Появился ремешок!</h1><br><b><a href="https://nomadgoods.com/products/sport-band-ultra-strike">Вот линка!</a></b>',
 }
+
+const buildMailOptions = ({ subject, html }) => ({
+  from: 'nodejs.257@gmail.com',
+  to:   'gdedat@gmail.com',
+  subject,
+  html,
+})
+
+mailer.sendMail(buildMailOptions(startOptions))
 
 const check = async () => {
   try {
@@ -18,7 +30,7 @@ const check = async () => {
     const available = json.product.variants.nodes[0].quantityAvailable
 
     if (available) {
-      mailer.sendMail(mailOptions)
+      await mailer.sendMail(buildMailOptions(successOptions))
     }
   } catch {
   }
