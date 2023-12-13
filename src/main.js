f35cf0538f266981c0c4e528e5b1939d17f2c738
Pavel Sturov <pavel.sturau@winlinetech.net>
2023-12-13 17:19:50 +0300

    Returned vercel.json

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import * as http from 'node:http'
-import { transporter } from './utils/mail-transporter.js'
+import mailTransporter from 'nodemailer'
+
+const user = 'nodejs.257@gmail.com'
+const pass = 'rbue qofn jtek ibot'
+
+const transporter = mailTransporter.createTransport({
+  service: 'Gmail',
+  host:    'smtp.gmail.com',
+  port:    465,
+  secure:  true,
+  auth:    { user, pass },
+})
 
 const MIME_TYPES = {
   default: 'application/octet-stream',
